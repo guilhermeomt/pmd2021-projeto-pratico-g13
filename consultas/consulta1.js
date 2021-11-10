f3a02fe01b2a9a799f9b04c1d4c409722e84d734
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2021-11-10 08:41:42 -0300

    Consulta 1 retornando o primeiro pais corrigido

--- consultas/consulta1.js
+++ consultas/consulta1.js
@@ -1,5 +1,5 @@
 var mapFunction = function () {
-  if(this.category === "A") {
+  if (this.category === "A") {
     emit(this.country, { count: 1 });
   }
 };
@@ -8,13 +8,15 @@ var reduceFunction = function (key, values) {
   var value = { count: 0 };
 
   values.forEach((v) => {
-      value.count += v.count;
+    value.count += v.count;
   });
 
   return value;
 };
 
 db.universities.mapReduce(mapFunction, reduceFunction, {
-  out: { inline: 1 },
+  out: "tmp_country_ranking",
   sort: { country: 1 },
+  
 });
+db.tmp_country_ranking.find().limit(1).pretty();
